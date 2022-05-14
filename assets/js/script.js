986e9382d7e014a1010c114e656d6a2b55f11e70
-l <dbonineiv@gmail.com>
2022-05-14 14:05:00 -0700

    added 'proxy'to fix getToken fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,12 @@ const cityInput = document.querySelector("#city-input");
 var autocomplete;
 var place;
 var userlocation;
+var locationCity;
+var locationState;
+var locationLat;
+var locationLng;
+var locationPost;
+
 function initMap() {
   var autocomplete = new google.maps.places.Autocomplete(cityInput, {
     componentRestrictions: { country: "us" },
@@ -13,55 +19,61 @@ function initMap() {
   autocomplete.addListener("place_changed", function () {
     place = autocomplete.getPlace();
     console.log(place.name, place.geometry.location, place.place_id);
+    locationCity = place.name;
   });
 }
 
-// //Search button
+//Search button
 
-// // fetch token on page load
-// function getToken() {
-//   fetch(
-//     "https://api.petfinder.com/v2/oauth2/token?grant_type=client_credentials&client_id=yZJnxnm7MRDykndmWxlpmmsUeSOcn0MwkYF7nE1CpCVnBmztQF&client_secret=3knFu1WvX735C8SFKEC3gwbvGKkMuCoR6w0wcUoQ"
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         //     {
-//         //   "token_type": "Bearer",
-//         //   "expires_in": 3600,
-//         //   "access_token": "..."
-//         // }
-//         token = data.access_token;
-//         epiry = Date.now() + data.expires_in;
-//         cb()
-//       })
-//   );
-// }
-// //curl -d "grant_type=client_credentials&client_id=yZJnxnm7MRDykndmWxlpmmsUeSOcn0MwkYF7nE1CpCVnBmztQF&client_secret=3knFu1WvX735C8SFKEC3gwbvGKkMuCoR6w0wcUoQ" https://api.petfinder.com/v2/oauth2/token
-// function getOrg() {
-//   fetch(
-//     "https://api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}",
-//     //https://api.petfinder.com/v2/organizations
-//     {
-//       headers: {
-//         Authorization: "Bearer" + token,
-//       },
-//     }
-//   )
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     });
-// }
-//  function apiRequest(cb){
-//    if(token === undefined || Date.now() > expiry){
-//      getToken(cb);
-//    }else{
-//      cb();
-//    }
-//  }
+// fetch token on page load
+var token;
+var expiry;
+function getToken(cb) {
+  fetch(
+    "proxy",
+    "https://api.petfinder.com/v2/oauth2/token?grant_type=client_credentials&client_id=yZJnxnm7MRDykndmWxlpmmsUeSOcn0MwkYF7nE1CpCVnBmztQF&client_secret=3knFu1WvX735C8SFKEC3gwbvGKkMuCoR6w0wcUoQ"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //     {
+      //   "token_type": "Bearer",
+      //   "expires_in": 3600,
+      //   "access_token": "..."
+      // }
+      token = data.access_token;
+      expiry = Date.now() + data.expires_in;
+      console.log("---------------------------------------");
+      console.log(data);
+      cb();
+    });
+}
+//curl -d "grant_type=client_credentials&client_id=yZJnxnm7MRDykndmWxlpmmsUeSOcn0MwkYF7nE1CpCVnBmztQF&client_secret=3knFu1WvX735C8SFKEC3gwbvGKkMuCoR6w0wcUoQ" https://api.petfinder.com/v2/oauth2/token
+function getOrg() {
+  fetch(
+    // "https://api.petfinder.com/v2/organizations/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}",
+    "https://api.petfinder.com/v2/organizations?location=" + locationCity,
+    {
+      headers: {
+        Authorization: "Bearer" + token,
+      },
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+function apiRequest(cb) {
+  if (token === undefined || Date.now() > expiry) {
+    getToken(cb);
+  } else {
+    cb();
+  }
+}
 
-//  getToken():
-//  apiRequest(getOrg)
+getToken();
+apiRequest(getOrg);
